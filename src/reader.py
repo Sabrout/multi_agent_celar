011825b232890bbf3b6dee55d93fd769ffd4e265
Sabrout <omarsabrout@gmail.com>
2018-12-30 17:55:43 +0100

    implementing the reader

---
+++ src/reader.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import glob
+
+
+class Reader():
+    def __init__(self, nbScen=1):
+        # Problem Name
+        nbScen = 'scen{0:02}'.format(nbScen)
+
+        result_file = open()
+
+
+def main():
+    print()
+
+
+if __name__ == "__main__":
+    main()
